2992030df288aa7547f7d08a4ebdc3fd8d40efae
Vasilii <goooneoooogle@gmail.com>
2021-01-19 00:14:40 +0200

    [ADD]: srction contacts

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,12 @@
 $(function() {
-	
+	$('input').on('click', function() {
+    $('.gradient-input').removeClass('isFocus');
+    $('.gradient-textarea').removeClass('isFocus');
+    $(this).parent().addClass('isFocus');
+  });
+  $('textarea').on('click', function() {
+    $('.gradient-input').removeClass('isFocus');
+    $('.gradient-textarea').removeClass('isFocus');
+    $(this).parent().addClass('isFocus');
+  });
 });
