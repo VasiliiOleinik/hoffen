483b855defc9908f9641551328d04eb39df735f4
Vasilii <goooneoooogle@gmail.com>
2021-01-20 23:35:36 +0200

    [continue]

--- app/js/common.js
+++ app/js/common.js
@@ -9,4 +9,13 @@ $(function() {
     $('.gradient-textarea').removeClass('isFocus');
     $(this).parent().addClass('isFocus');
   });
+
+  $('#completed-carousel-js').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: $('.completed-prev-arrow'),
+    nextArrow: $('.completed-next-arrow'),
+  });
 });
