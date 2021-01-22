c37cd0f223a94aed2d9fa72299f376f65cd1e905
Vasilii <goooneoooogle@gmail.com>
2021-01-23 01:50:48 +0200

    [pages]

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,10 @@
-$(function() {
-	$('input').on('click', function() {
+$(function () {
+  $('input').on('click', function () {
     $('.gradient-input').removeClass('isFocus');
     $('.gradient-textarea').removeClass('isFocus');
     $(this).parent().addClass('isFocus');
   });
-  $('textarea').on('click', function() {
+  $('textarea').on('click', function () {
     $('.gradient-input').removeClass('isFocus');
     $('.gradient-textarea').removeClass('isFocus');
     $(this).parent().addClass('isFocus');
@@ -17,20 +17,64 @@ $(function() {
     autoplaySpeed: 2000,
     prevArrow: $('.completed-prev-arrow'),
     nextArrow: $('.completed-next-arrow'),
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+    ]
   });
-  $('.review-nav_item').each(function(index) {
+  $('.review-nav_item').each(function (index) {
     $(this).attr('data-idx', ++index);
   });
-  $('.review-nav_item').on('click', function() {
+  $('.review-nav_item').on('click', function () {
     const order = $(this).attr('data-idx');
     $('.review-nav_item').removeClass('active');
     $(this).addClass('active');
-    $('.review-item').css({'order': 2});
-    $(`.review-item:nth-child(${order})`).css({'order': -5});
+    $('.review-item').css({ 'order': 2 });
+    $(`.review-item:nth-child(${order})`).css({ 'order': -5 });
+    if (screen.width < 992) {
+      $('.review-item').hide();
+      $(`.review-item:nth-child(${order})`).show().css({ 'display': 'flex' });
+    }
   });
-  $('.hover-box').hover(function() {
+  $('.hover-box').hover(function () {
     $(this).find('.hover-box-hover').fadeIn(200);
-  }, function() {
+  }, function () {
     $(this).find('.hover-box-hover').fadeOut(200);
   });
+  $('.mobile-left-nav-btn').on('click', function () {
+
+    $(this).addClass('active');
+    $(this).text('Close menu');
+    $('.main-left-nav').addClass('active');
+  });
+  $(document).mouseup(function (e) {
+    var div = $(".main-left-nav.active");
+    var btn = $(".mobile-left-nav-btn.active");
+    if (!div.is(e.target) && div.has(e.target).length === 0) {
+      div.removeClass('active');
+      btn.removeClass('active');
+    }
+    // if(btn.is(e.target)) {
+    //   div.removeClass('active');
+    //   btn.removeClass('active');
+    // }
+  });
+
+
 });
